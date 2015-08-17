5220b970c6d22a720a374a2067238e0e2e65914b
jjutrilla <jjutrilla.desa@gmail.com>
2015-08-17 21:44:39 +0200

    Gestion de listas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -33,7 +33,8 @@ exports.index = function(req, res) {
 			function(quizes) {
 				res.render('quizes/index',
 					{
-						quizes: quizes
+						quizes: quizes,
+						errors:	[] 
 					}
 				);
 			}
@@ -45,7 +46,7 @@ exports.index = function(req, res) {
 	} else {
 		models.Quiz.findAll().then(
 			function(quizes) {
-				res.render('quizes/index', {quizes: quizes});
+				res.render('quizes/index', {quizes: quizes, errors: []});
 			}
 		).catch(
 			function(error) {
@@ -57,7 +58,7 @@ exports.index = function(req, res) {
 
 // GET /quizId/:id
 exports.show = function(req, res) {
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 // GET /quizId/:id/answer
@@ -71,7 +72,113 @@ exports.answer = function(req, res) {
 	res.render('quizes/answer',
 		{
 			quiz:		req.quiz, 
-			respuesta:	resultado
+			respuesta:	resultado,
+			errors:		[]
+		}
+	);
+};
+
+// GET /quizes/new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(	// crea objeto quiz
+		{
+			pregunta:	"Pregunta",
+			respuesta:	"Respuesta" 
+		}
+	);
+	
+	res.render('quizes/new',
+		{
+			quiz:	quiz,
+			errors:	[]
+		}
+	);
+};
+
+// POST /quizes/create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz );
+	
+	quiz
+	.validate()
+	.then(
+		function(err) {
+			if (err) {
+				res.render('quizes/new',
+				{
+					quiz:	quiz,
+					errors:	err.errors  
+				});
+			} else {
+				// guarda en BD los campos pregunta y respuesta de quiz
+				quiz
+				.save(
+					{
+						fields: ["pregunta", "respuesta"]
+					})
+				.then(
+					function()
+					{
+						res.redirect('/quizes')
+					}
+				)	// Redirección HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);
+};
+
+// GET /quizId/:id/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz;	// Autoload de instancia de quiz
+	
+	res.render('quizes/edit',
+		{
+			quiz:		quiz,
+			errors:		[]
+		}
+	);
+};
+
+// PUT /quizes/:id
+exports.update = function(req, res) {
+	req.quiz.pregunta  = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err)
+		{
+			if (err) {
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				req.quiz     // save: guarda campos pregunta y respuesta en DB
+				.save( {fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes');});
+			}     // Redirección HTTP a lista de preguntas (URL relativo)
+    	}
+  	)
+  	.catch(
+  		function(error)
+  		{
+  			next(error)
+  		}
+  	);
+};
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy()
+	.then(
+		function()
+		{
+			res.redirect('/quizes');
+		}
+	)
+	.catch(
+		function(error)
+		{
+			next(error);
 		}
 	);
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,9 +1,19 @@
 // Definiciomn del modelo de Quiz
 
 module.exports = function(sequelize, DataTypes) {
-	return sequelize.define('Quiz', 
-	{
-		pregunta: DataTypes.STRING,
-		respuesta: DataTypes.STRING
-	});
+	return sequelize.define(
+		'Quiz', 
+		{
+			pregunta:
+			{
+				type:	DataTypes.STRING,
+				validate:	{ notEmpty: {msg: "-> Falta pregunta"}} 
+			},
+			respuesta:
+			{
+				type:	DataTypes.STRING,
+				validate:	{ notEmpty: {msg: "-> Falta respuesta"}}
+			}
+		}
+	);
 }
